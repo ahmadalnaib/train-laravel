3eefe56a94442009f30de25a2a3aa8cda2a4aa2b
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-15 11:03:53 +0100

    add navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Laravel || Auth</title>
+</head>
+<body class="bg-gray-100">
+
+@include('layouts.navbar')
+
+<div class="container mx-auto mt-6 px-6">
+    @yield('content')
+</div>
+
+</body>
+</html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="flex justify-between bg-white  p-6">
+  <ul class="flex items-center">
+      <li><a class="p-3" href="#">Home</a></li>
+      <li><a href="{{route('dashboard')}}">Dashboard</a></li>
+  </ul>
+
+    <ul class="flex items-center">
+        <li><a class="p-3" href="#">Ahmed</a></li>
+        <li><a class="p-3" href="#">Register</a></li>
+        <li><a class="p-3" href="#">Login</a></li>
+        <li><a class="p-3" href="#">Logout</a></li>
+    </ul>
+</nav>
